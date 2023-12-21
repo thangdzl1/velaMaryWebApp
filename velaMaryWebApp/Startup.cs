11f1f6a7a2984ff1781acbeefade25ba6091cbc8
BQcThang <quocthang6a1@gmail.com>
2023-12-21 15:21:45 +0700

    initial commit

---
+++ velaMaryWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(velaMaryWebApp.Startup))]
+namespace velaMaryWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
